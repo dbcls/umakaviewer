647fa586295c8bd4b052dc768b6f331f9a9abd57
Riku Yamauchi <ripry258@gmail.com>
2021-01-21 18:20:04 +0900

    Add common logic for filtering content and filter Graph

--- node/src/ts/visualizer/utils/BlackList.ts
+++ node/src/ts/visualizer/utils/BlackList.ts
@@ -1,6 +1,3 @@
-import { AppState } from '..'
-import { Classes } from '../types/class'
-import { Structure } from '../types/structure'
 import { readTextFileContent } from '.'
 import { Prefixes } from '../types/prefix'
 
@@ -29,46 +26,6 @@ class Blacklist {
             (item) => prefix === item || prefix.startsWith(item)
           )))
     )
-  }
-
-  filter({ structure, classes, properties, prefixes }: AppState): AppState {
-    const filterStructure = (children: Structure[]) => {
-      for (let i = 0; i < children.length; i += 1) {
-        const node = children[i]
-        if (this.has(node.uri, prefixes)) {
-          children.splice(i, 1)
-          i -= 1
-        } else if (node.children !== undefined) {
-          filterStructure(node.children)
-        }
-      }
-    }
-    filterStructure(structure)
-
-    const filteredClasses = Object.keys(classes).reduce<Classes>((obj, uri) => {
-      if (this.has(uri, prefixes)) {
-        return obj
-      }
-      obj[uri] = classes[uri] // eslint-disable-line no-param-reassign
-      return obj
-    }, {})
-
-    properties.forEach(({ class_relations: relations }) => {
-      for (let i = 0; i < relations.length; i += 1) {
-        const { subject_class: s, object_class: o } = relations[i]
-        if ((s && this.has(s, prefixes)) || (o && this.has(o, prefixes))) {
-          relations.splice(i, 1)
-          i -= 1
-        }
-      }
-    })
-
-    return {
-      structure,
-      classes: filteredClasses,
-      properties,
-      prefixes,
-    }
   }
 }
 
