9adfe41a4b9d6671b4fdf381688e4ff64663a9f4
Riku Yamauchi <ripry258@gmail.com>
2021-01-25 14:56:58 +0900

    Change input position of filtering

--- node/src/ts/visualizer/components/Filter.tsx
+++ node/src/ts/visualizer/components/Filter.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback } from 'react'
+import { useIntl } from 'react-intl'
 import { useDispatch } from 'react-redux'
 import { DetailAction } from '../actions/detail'
 import { FilterAction } from '../actions/filter'
 
 const Filter: React.FC = () => {
   const dispatch = useDispatch()
+  const intl = useIntl()
 
   const handleKeyPress = useCallback(
     (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -21,8 +23,32 @@ const Filter: React.FC = () => {
   )
 
   return (
-    <div id="filter">
-      <input type="number" onKeyPress={handleKeyPress} maxLength={4} />
+    <div id="filter-wrapper">
+      <div className="filter-header">
+        <span className="legend-label">
+          {intl.formatMessage({
+            id: 'filter.display.condition',
+          })}
+        </span>
+        <button type="button" className="hidden-toggle" />
+      </div>
+      <div className="filter-conditions">
+        <ul>
+          <li>
+            <span>
+              {intl.formatMessage({
+                id: 'filter.show.more.than.specified.entities.prefix',
+              })}
+            </span>
+            <input type="number" onKeyPress={handleKeyPress} />
+            <span>
+              {intl.formatMessage({
+                id: 'filter.show.more.than.specified.entities.suffix',
+              })}
+            </span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
